a7fe09e82d7f2cfa236a5b6f5abc90c381f1c311
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2024-08-24 13:17:40 +0200

    ...

--- Assets/Scripts/CMD/OpenCmd.cs
+++ Assets/Scripts/CMD/OpenCmd.cs
@@ -3,24 +3,25 @@ using System.Diagnostics;
 
 public class OpenCmd : MonoBehaviour
 {
-    // This function will be called when you want to open CMD
+    void Start()
+    {
+        OpenCommandPrompt();
+    }
+
     public void OpenCommandPrompt()
     {
         try
         {
-            // Create a new process start info for CMD
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
-                FileName = "cmd.exe", // Command to open CMD
-                UseShellExecute = true // Use the OS shell to execute the command
+                FileName = "cmd.exe",     
+                UseShellExecute = true         
             };
 
-            // Start the process
             Process.Start(startInfo);
         }
         catch (System.Exception ex)
         {
-            // Handle exceptions if needed
             UnityEngine.Debug.LogError($"Failed to open CMD: {ex.Message}");
         }
     }
